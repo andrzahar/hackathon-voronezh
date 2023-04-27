80a888c5d5e44fe044bcce36ac392636d8382755
stalk <stalkeronline2016@mail.ru>
2023-04-28 02:33:05 +0300

    hotfix

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -7,12 +7,14 @@ import {
   MethodNotAllowedException,
   Patch,
   Post,
-  Request
+  Request, UseGuards
 } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { UserUpdateDto } from "./dto/user-update.dto";
 import { UserDeleteDto } from "./dto/user-delete.dto";
 import { RegistrationDto } from "../registration/dto/registration.dto";
+import { RolesGuard } from "../auth/roles.guard";
+import { Roles } from "../auth/roles-auth.decorator";
 
 @Controller('user')
 export class UsersController {
@@ -23,22 +25,38 @@ export class UsersController {
     return await this.usersService.findOneByMail(req.user.mail)
   }
 
-  @Post('/create')
+  @HttpCode(200)
+  @Post()
   public async createUser(@Body() dto: RegistrationDto) {
-    return this.usersService.create(dto)
-  }
-
-  @Patch('/update')
-  public async updateUser(@Body() dto: UserUpdateDto) {
-    return null
-  }
-
-  @Delete('/delete')
-  public async deleteUser(@Body() dto: UserDeleteDto) {
-    return await this.usersService.delete(dto)
+    try {
+      return this.usersService.create(dto)
+    } catch {
+      throw new MethodNotAllowedException()
+    }
   }
 
   @HttpCode(200)
+  @Patch()
+  public async updateUser(@Body() dto: UserUpdateDto) {
+    try {
+      return this.usersService.update(dto)
+    } catch {
+      throw new MethodNotAllowedException()
+    }
+  }
+
+  @Delete()
+  public async deleteUser(@Body() dto: UserDeleteDto) {
+    try {
+      return this.usersService.delete(dto)
+    } catch {
+      throw new MethodNotAllowedException()
+    }
+  }
+
+  @HttpCode(200)
+  @UseGuards(RolesGuard)
+  @Roles('Администратор ФСП')
   @Post('/role')
   public async addRole(@Request() req) {
      try {
