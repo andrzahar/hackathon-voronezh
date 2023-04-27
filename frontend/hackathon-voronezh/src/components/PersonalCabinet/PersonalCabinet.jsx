427744bb5d91066efafe60a80e245f24834c922d
Рома Адамович <roman-adamovich@mail.ru>
2023-04-27 19:32:00 +0500

    upgrade personal cabinet

--- frontend/hackathon-voronezh/src/components/PersonalCabinet/PersonalCabinet.jsx
+++ frontend/hackathon-voronezh/src/components/PersonalCabinet/PersonalCabinet.jsx
@@ -1,13 +1,40 @@
 import classes from './PersonalCabinet.module.css';
 
-const PersonalCabinet=()=>{
-    return(
-        <div>
-            <div className={classes.personalData}>
-                <div className={classes.personalData_img}>
-                    <img className={classes.avatar}/>
-                </div>
+const PersonalCabinet = () => {
+    return (
+        <div className={classes.personalCabinet}>
+            <h1 className={classes.title}>
+                Личный кабинет
+            </h1>
+            <div className={classes.personal}>
+                <div className={classes.personal_img}>
+                    <img className={classes.avatar}
 
+                    />
+                </div>
+                <div className={classes.personalData}>
+                    <div className={classes.basicData}>
+                        <p>Фамилия: </p>
+                        <p>Имя: </p>
+                        <p>Отчество: </p>
+                        <p>Дата рождения: </p>
+                    </div>
+                    <div className={classes.contacts}>
+                        <p>Телефон: </p>
+                        <p>Пол: </p>
+                        <p>Локация: </p>
+                    </div>
+                </div>
+            </div>
+            <div className={classes.personalInfo}>
+                <h2 className={classes.personalInfo__title}>
+                    О себе
+                </h2>
+                <div className={classes.personalInfoWrapper}>
+                    <p className={classes.personalInfo__text}>
+                        Я такой-то такой-то, то-то, то-то
+                    </p>
+                </div>
             </div>
         </div>
     )
