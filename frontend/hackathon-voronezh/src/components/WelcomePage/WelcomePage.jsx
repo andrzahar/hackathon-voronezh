af23f454fa8e2bd5c1a6a6f5036e989f8b4b0b71
Aleksandr Tsven <86127025+Ikiso@users.noreply.github.com>
2023-04-27 11:52:51 +0300

    Delete frontend directory

--- frontend/hackathon-voronezh/src/components/WelcomePage/WelcomePage.jsx
+++
@@ -1,8 +0,0 @@
-const WelcomePage=()=>{
-    return(
-        <>
-        </>
-    )
-}
-
-export default WelcomePage;
--- frontend/hackathon-voronezh/src/styles/COLORS.js
+++
@@ -1,5 +0,0 @@
-export const BLUE="#402FFF"
-export const RED="#EC1D35"
-export const GREY="#C8C9CA"
-export const DARKGREY="#2D2E37"
-export const BLACK="#1B1C21"
