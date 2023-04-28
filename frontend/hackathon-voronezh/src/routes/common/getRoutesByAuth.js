107838dfe48f4377cb66203915297010c191bd52
Marina.Tkachenko <90363353+ferimasv@users.noreply.github.com>
2023-04-28 07:59:07 +0500

    вроде что-то работало

--- frontend/hackathon-voronezh/src/routes/common/getRoutesByAuth.js
+++ frontend/hackathon-voronezh/src/routes/common/getRoutesByAuth.js
@@ -1,6 +1,7 @@
 import { AUTH_ROUTES, BASE_ROUTES } from '../baseRoutes';
 
 export const getRoutesByAuth = user => {
+  console.log('user.token', user);
   if (user.token) {
     return BASE_ROUTES;
   }
--- frontend/hackathon-voronezh/src/store/reducers/userReducer.js
+++ frontend/hackathon-voronezh/src/store/reducers/userReducer.js
@@ -4,13 +4,31 @@ const initialState = {
     surName: 'Адамович',
     patronymic: 'Николаевич',
     login: 'RNOre',
-    token: 'undefined',
+    token: undefined,
     phone: '89877807652',
     role: 'Спортсменd'//enum
 };
 
 export const userReducer = (state = initialState, action) => {
-    switch (action?.type) {
+    console.log('state', state)
+    console.log('action', action)
+    switch (action.type) {
+        case 'SET_TOKEN': {
+            const newState = {
+                ...state,
+                token: action.payload.token,
+            };
+
+            return newState;
+        }
+        case 'SET_USER_INFO': {
+            const newState = {
+                ...action.payload.user,
+                token: action.payload.token,
+            };
+
+            return newState;
+        }
         default:
             return state;
     }
