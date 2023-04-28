2c5ec07f03d04848acc7b6cac7cbeeee64166878
Рома Адамович <roman-adamovich@mail.ru>
2023-04-28 10:41:26 +0500

    fix route bag

--- frontend/hackathon-voronezh/src/routes/baseRoutes.js
+++ frontend/hackathon-voronezh/src/routes/baseRoutes.js
@@ -3,7 +3,7 @@ import {
   ROUTE_ACHIEVEMENTS,
   ROUTE_ACHIEVEMENTS_ITEM,
   ROUTE_APPROVE,
-  ROUTE_AUTH,
+  ROUTE_AUTH, ROUTE_FAQ,
   ROUTE_MAIN_CONTAINER
 } from './routes';
 import Calendar from '../components/Calendar/Calendar';
@@ -12,12 +12,18 @@ import PersonalCabinet from '../components/PersonalCabinet/PersonalCabinet';
 import AchievementsListPage from '../components/Achievements/AchievementsListPage';
 import Approve from "../components/Approve/Approve";
 import AchievementsPage from "../components/Achievements/AchievementsPage";
+import FAQ from "../components/FAQ/FAQ";
 
 export const BASE_ROUTES = [
   {
     path: ROUTE_MAIN_CONTAINER,
     name: 'Главное меню',
     Element: Calendar,
+  },
+  {
+    path: ROUTE_FAQ,
+    name: 'Личный кабинет',
+    Element: FAQ,
   },
   {
     path: ROUTE_ACCOUNT,
